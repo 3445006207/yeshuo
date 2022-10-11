f04edf816172c391b52b3c940edc1257c2b3122c
3445006207 <98775662+3445006207@users.noreply.github.com>
2022-10-11 19:12:55 +0800

    Create gdcs.js

---
+++ gdcs.js
@@ -0,0 +1,25 @@
+/******************************
+自用测试 QQ3445006207
+******************************
+[rewrite_local]
+^https:\/\/uu\.tuanyougou\.com\/\/video url script-response-body https://raw.githubusercontent.com/3445006207/-/main/gdcs.js
+[mitm] 
+hostname = uu.tuanyougou.com
+*****************************/
+
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
+
+const cs = '/registryUser';
+
+
+if (url.indexOf(cs) != -1) {
+    obj.data.isVip = 1;
+obj.data.userType = 1;
+obj.data.endTime = "2999-9-9";
+body = JSON.stringify(obj);
+}
+
+
+$done({body});
